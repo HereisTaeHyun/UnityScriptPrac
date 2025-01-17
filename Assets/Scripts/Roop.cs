156247a6f78f1a53f9eab56139da0656009e2f30
HereisLuvika <kth882500@gmail.com>
2025-01-17 14:47:40 +0900

    RoopWatch

--- Assets/Scripts/Roop.cs
+++ Assets/Scripts/Roop.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Roop : MonoBehaviour
@@ -8,6 +9,12 @@ public class Roop : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // ForLoop();
+        ForEach();
+    }
+
+    void ForLoop()
+    {
         for(int i = 1; i <= numEnemies; i++)
         {
             Debug.Log($"Creating Enemy : {i}");
@@ -15,6 +22,55 @@ public class Roop : MonoBehaviour
         for(int i = numEnemies; i >= 0; i--)
         {
             Debug.Log($"Deleting Enemy : {i}");
+        }
+    }
+
+    void WhileLoop()
+    {
+        int cupInSink = 4;
+        while (cupInSink > 0)
+        {
+            Debug.Log($"cupInSinkNow : {cupInSink}");
+            cupInSink--;
+        }
+
+        Debug.Log("For version");
+
+        cupInSink = 4;
+        for(int i = cupInSink; i > 0; i--)
+        {
+            Debug.Log($"cupInSinkNow : {i}");
+        }
+    }
+
+    void DoWhileLoop()
+    {
+        bool shouldContinue = false;
+
+        do
+        {
+            Debug.Log("Hello");
+        } while(shouldContinue == true);
+    }
+
+    void ForEach()
+    {
+        string[] strings = new string[3];
+
+        strings[0] = "First";
+        strings[1] = "Second";
+        strings[2] = "Third";
+
+        foreach(var elem in strings)
+        {
+            Debug.Log(elem);
+        }
+
+        Debug.Log("For version");
+
+        for(int i = 0; i < strings.Length; i++)
+        {
+            Debug.Log(strings[i]);
         }
     }
 
--- Assets/Scripts/VariableType.cs
+++ Assets/Scripts/VariableType.cs
@@ -1,11 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Diagnostics;
+using Debug = UnityEngine.Debug;
 
 public class VariableType : MonoBehaviour
 {
     public bool staticVariable = true;
-    // private int mGlovalVariable = 1;
+    private int mGlovalVariable = 1;
     public int abCde = 1; // 유니티 에디터 상으로 조작 가능
     public string STR = "Hi, World";
     const float pi = 3.14f;
@@ -18,18 +21,44 @@ public class VariableType : MonoBehaviour
         // Debug.Log(localVariable2.ToString());
         // LocalVariable(1);
 
-        int maxInt = int.MaxValue;
-        int minInt = int.MinValue;
+        // int maxInt = int.MaxValue;
+        // int minInt = int.MinValue;
 
-        Debug.Log($"abcde : {abCde}");
-        Debug.Log($"STR : {STR}");
-        Debug.Log($"maxInt : {maxInt}");
-        Debug.Log($"minInt : {minInt}");
+        // Debug.Log($"abcde : {abCde}");
+        // Debug.Log($"STR : {STR}");
+        // Debug.Log($"maxInt : {maxInt}");
+        // Debug.Log($"minInt : {minInt}");
+
+        // int? nullInt = null;
+
+        TimeSpan();
     }
 
     void LocalVariable(int param)
     {
         float localVariable = param;
+    }
+
+    void TimeSpan()
+    {
+        Stopwatch stopwatch = new Stopwatch();
+        DateTime dt1 = new DateTime(2018, 08, 01, 02, 03, 04);
+        DateTime dt2 = new DateTime(2019, 10, 20, 12, 00, 05);
+
+        TimeSpan ts = dt2 - dt1;
+
+        // 둘 사이 단순 마이너스 시간 차이
+        stopwatch.Start();
+        Debug.Log(ts.Days.ToString());
+        Debug.Log(ts.Hours.ToString());
+        Debug.Log(ts.Minutes.ToString());
+        Debug.Log(ts.Seconds.ToString());
+
+        // 둘 사이 실제 모든 지나간 시간 차이
+        Debug.Log(ts.TotalHours.ToString());
+        Debug.Log(ts.TotalMinutes.ToString());
+        stopwatch.Stop();
+        Console.WriteLine($"경과 시간 {stopwatch.ElapsedMilliseconds}");
     }
 
     // Update is called once per frame
